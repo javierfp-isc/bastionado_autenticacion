f76c4cf5f6eea30010c74e774db3cdd8a190a14d
Javier Fernández Peón <javierfp@iessanclemente.net>
2021-08-07 16:16:12 +0200

    kerberos

--- kerberos/compose/build/entrypoint.sh
+++ kerberos/compose/build/entrypoint.sh
@@ -6,6 +6,9 @@ echo "192.168.98.100 kdc kdc.auth.kerb" >> /etc/hosts
 #Arranca kerberos kdc
 [ -d /var/kerberos ] && service krb5-kdc start
 
+#Arranca ssh
+[ -f /etc/ssh/sshd_config ] && service ssh start 
+
 #Uso exec para lanzar un proceso independiente de bucle infinito
 exec bash -c "while true;do sleep 10;done"
 
